586b80cf1324363842a79c0277475e8d92214c82
Oussama KADDAMI <kaddami@kth.ug.se>
2022-02-13 08:05:11 +0100

    Implementation of the key expansion

--- encrypt.c
+++ encrypt.c
@@ -1,5 +1,40 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "encrypt.h"
 
-void keyExpansion(char key[]){}
+#include "encrypt.h"
+#include "operations.h"
+
+
+
+unsigned char Rcon[10] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36};
+
+void keyExpansion(byte *key, word *w, int nk){
+    word temp;
+    int i = 0;
+
+    /* We copy the key into the first 4 bytes of w */
+    memcpy((byte *) w, (byte *) key, nk*wordSize);
+
+    /*** The difference between strncpy and memcpy
+     * strcpy copies C **Strings**, if you're copying bulk
+     * memory with random bytes in it, strncpy is
+     * just broken ***/
+    
+    //assert
+    i = nk;
+    byte rcon[4] = {0, 0, 0, 0};
+    while(i < size*(numRounds + 1)){
+        temp = w[i -1];
+        if (i%nk == 0){
+            rcon[0] = Rcon[i/nk - 1]; 
+            rotWord(&temp);
+            subWord(&temp);
+            wordXor(&temp, &temp, (word *) rcon);
+        } else if ((nk > 6 ) & (i%nk == size)){
+            subWord(&temp);
+        }
+        wordXor(w + i, w + i -nk, &temp);
+        i++;
+    }
+}
--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "encrypt.h"
+#include "operations.h"
+
+char *inFile = "in/aes_sample.in\0";
+int size = 4; /** This is Nk = Nb **/
+int wordSize = 4; /* We consider words of 32 bits = 4 bytes */
+int numRounds = 10; /* Number of rounds for AES-128 is 10 **/
 
 /** Global variables are initialized by the
  * compiler itself and should be initialized
@@ -16,22 +22,19 @@ void main(int argc, char **argv){
     /** Open the in file **/
     FILE *fp = fopen(inFile, "r");
     /*** Extract the key ***/
-    char key[size*wordSize];
-    fread(key, sizeof(char), size, fp);
-    printf("%lx\n", *((long unsigned int *) key));
-    printf("%lx\n", *((long unsigned int *) key + 1));
+    byte key[size*wordSize];
+    fread(key, 1, size*wordSize, fp);
 
     /** We need to read the blocks to encrypt; Each block
-     * is 16 bytes **/
+     * is of 16 bytes size**/
     int readSize = 1;
     char block[size*wordSize];
     while (readSize){
-        readSize = fread(block, sizeof(char), size, fp);
+        readSize = fread(block, 1, size*wordSize, fp);
         if (readSize){
             /* Here we need to apply the encryption and write it into
             the output file*/ 
-            printf("%lx\n", *((long unsigned int *) block));
-            printf("%lx\n", *((long unsigned int *) block + 1));
+
         }
     };
 }
